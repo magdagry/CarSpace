6b34e2ac4195616df38a0bb114e1ff8a4f212a5a
Lenka <magdagry16@gmail.com>
2023-03-30 21:38:02 +0200

    cars list

---
+++ details.js
@@ -0,0 +1,10 @@
+let kia = {
+    marka: kia,
+    model: sportage,
+    rocznik: 2021,
+    mocSilnika: 120Kh,
+    cena: 
+}
+
+b. Dane do wyświetlania: cena, obrazek, marka, rok, model, rocznik, moc silnika,
+przebieg.
